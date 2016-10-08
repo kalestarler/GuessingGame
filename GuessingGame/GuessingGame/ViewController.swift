b7cefad4dd8a844ee87980e46f646831443af39e
Kale <kale@TechX-MacBook-Pro-Kale.local>
2016-10-08 15:55:10 +0800

    Finished game!

--- GuessingGame/GuessingGame/ViewController.swift
+++ GuessingGame/GuessingGame/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
         inputTextField.isHidden = true
         guessButton.isHidden = true
         gameMessageLabel.isHidden = true
+        gameMessageLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,25 +45,65 @@ class ViewController: UIViewController {
         gameMessageLabel.isHidden = false
         startGameButton.isHidden = true
         
+        //resets number of tries for next round
+        numberOfTries = 0
+        
+        //generates new secret number
         secretNumber = generateRandomNumberUpTo(max: 20)
+        
+        print(secretNumber)
     }
     
     @IBAction func guessButtonAction(_ sender: AnyObject) {
         
-        let userInput = Int(inputTextField.text!)!
+        if inputTextField.text?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) == "" {
+            
+            //Did not enter anything
+            
+            gameMessageLabel.text = "You didn't enter a valid input"
+            
+            return
+        }
+        
+        guard let userInput = Int(inputTextField.text!) else {
+            
+            //no valid number typed in
+            
+            gameMessageLabel.text = "You didn't enter a valid input"
+            
+            return
+        }
+        
+        //resets text field for next round
+        inputTextField.text = ""
         
         if (userInput == secretNumber) {
          
+            numberOfTries = numberOfTries + 1
+            
             //Win! End game
-            gameMessageLabel.text = "Congratulations! You guessed it right!"
+            
+            if numberOfTries == 1 {
+                
+                gameMessageLabel.text = "Congratulations! You guessed it right! (\(numberOfTries) try)"
+            }
+            else {
+                
+                gameMessageLabel.text = "Congratulations! You guessed it right! (\(numberOfTries) tries)"
+            }
+            
+            startGameButton.setTitle("Start New Game", for: .normal)
+            startGameButton.isHidden = false
         }
         else if (userInput > secretNumber) {
             
             gameMessageLabel.text = "Your guess was too big!"
+            numberOfTries = numberOfTries + 1
         }
         else if (userInput < secretNumber) {
             
             gameMessageLabel.text = "Your guess was too small!"
+            numberOfTries = numberOfTries + 1
         }
     }
     
